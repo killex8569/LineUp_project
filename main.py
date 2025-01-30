81df853959cb10c1d655538f2e24dbd65d52efa8
killex8569 <alexandreanette@gmail.com>
2025-01-30 21:18:53 +0100

    add frames

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from customtkinter import *
 
 # set the default appearance
 customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("green")
+
 
 # deactivate automatic scaling
 customtkinter.deactivate_automatic_dpi_awareness()
@@ -12,13 +14,36 @@ customtkinter.deactivate_automatic_dpi_awareness()
 # ajout de l'interface
 lineup = CTk()
 lineup.geometry("600x500")
-
-login_frame = customtkinter.CTkFrame(master=lineup)
-login_frame.pack(pady=50, padx=50)
+lineup.title("LineUp")
 
 
-login_button = customtkinter.CTkButton(master=login_frame, text="connexion")
-login_button.pack(pady=12, padx=10)
+
+# Création de la frame (logo)
+content_frame = customtkinter.CTkFrame(master=lineup, fg_color="#1E90FF", corner_radius=0)
+content_frame.grid(row=0, column=0, sticky="nswe")
+content_frame.grid_columnconfigure(1, weight=1)
+content_frame.grid_rowconfigure(0, weight=1)
+
+# Création de la frame 2 (action)
+content_frame2 = customtkinter.CTkFrame(master=lineup, fg_color="#1E90FF", corner_radius=0)
+content_frame2.grid(row=1, column=0, sticky="nswe")
+content_frame2.grid_columnconfigure(1, weight=1)
+content_frame2.grid_rowconfigure(1, weight=1)
+
+# Création de la frame 3 (account)
+content_frame3 = customtkinter.CTkFrame(master=lineup, fg_color="#1E90FF", corner_radius=0)
+content_frame3.grid(row=2, column=0, sticky="nswe")
+content_frame3.grid_columnconfigure(1, weight=1)
+content_frame3.grid_rowconfigure(2, weight=1)
+
+
+# Création de la frame 4 (end)
+content_frame4 = customtkinter.CTkFrame(master=lineup, fg_color="#1E90FF", corner_radius=0)
+content_frame4.grid(row=3, column=0, sticky="nswe")
+content_frame4.grid_columnconfigure(1, weight=1)
+content_frame4.grid_rowconfigure(2, weight=1)
+
+# Menu latéral
 
 
 lineup.mainloop()
