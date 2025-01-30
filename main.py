841cc74c0921144bb93f66fdee17ea3f7a15474c
killex8569 <alexandreanette@gmail.com>
2025-01-30 20:31:04 +0100

    add frame

--- main.py
+++ main.py
@@ -3,9 +3,22 @@ import tkinter
 import customtkinter
 from customtkinter import *
 
+# set the default appearance
+customtkinter.set_appearance_mode("dark")
+
+# deactivate automatic scaling
+customtkinter.deactivate_automatic_dpi_awareness()
+
 # ajout de l'interface
 lineup = CTk()
 lineup.geometry("600x500")
 
-# test
+login_frame = customtkinter.CTkFrame(master=lineup)
+login_frame.pack(pady=50, padx=50)
+
+
+login_button = customtkinter.CTkButton(master=login_frame, text="connexion")
+login_button.pack(pady=12, padx=10)
+
+
 lineup.mainloop()
