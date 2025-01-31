456aa0b3fbbd09b6cc146caa2669ee8e6f159624
valentinRyckaert <v.ryckaert@eleve.leschartreux.net>
2025-02-01 00:26:07 +0100

    update daoUser

--- db/daos/DAOUser.py
+++ db/daos/DAOUser.py
@@ -1,4 +1,13 @@
 import json
 
 class DAOUser:
-    ...
+    dbPath = None
+
+    def __init__(self, path):
+        self.dbPath = path
+
+    def getAll(self):
+        with open(self.dbPath, 'r') as db:
+            data: list[Playlist] = []
+            print(json.load(db))
+        return data
