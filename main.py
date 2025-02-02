c13118658f2f1fb00a001f9ec47e3f1f0aa84414
killex8569 <alexandreanette@gmail.com>
2025-02-02 21:44:19 +0100

    adding image better GUI V1.02

--- main.py
+++ main.py
@@ -22,6 +22,8 @@ lineup.geometry("700x600")
 lineup.title("LineUp")
 lineup.iconbitmap("data/LineUp.ico")
 lineup.resizable(False, False)
+
+# font
 font_path = "/data/Kanit/Kanit-Regular.ttf"
 lineup_font = customtkinter.CTkFont(family="Kanit", size=16)
 lineup.option_add("*font", lineup_font)
@@ -46,17 +48,25 @@ content_frame2.grid(row=1, column=0, sticky="nswe")
 content_frame2.grid_columnconfigure(1, weight=1)
 content_frame2.grid_rowconfigure(1, weight=1)
 
+# img for button
+setting_png = customtkinter.CTkImage(Image.open("data/setting.png"), size=(20, 20))
+account_png = customtkinter.CTkImage(Image.open("data/account.png"), size=(20, 20))
+playlist_png = customtkinter.CTkImage(Image.open("data/playlist.png"), size=(20, 20))
+download_png = customtkinter.CTkImage(Image.open("data/download-circular-button.png"), size=(20, 20))
+youtube_png = customtkinter.CTkImage(Image.open("data/youtube.png"), size=(20, 20))
+
+
 # bouton frame 2
-btn_youtube = customtkinter.CTkButton(master=content_frame2, text="Youtube", width=150, height=50, text_color="white", font=lineup_font, fg_color="transparent")
+btn_youtube = customtkinter.CTkButton(master=content_frame2, text="Youtube", width=150, height=50, text_color="white", font=lineup_font, fg_color="transparent", image=youtube_png)
 btn_youtube.grid(row=1, column=1, pady=25, padx=40)
-btn_url_download = customtkinter.CTkButton(master=content_frame2, text="URL download", width=150, height=50, text_color="white", font=lineup_font, fg_color="transparent")
+btn_url_download = customtkinter.CTkButton(master=content_frame2, text="URL download", width=150, height=50, text_color="white", font=lineup_font, fg_color="transparent", image=download_png)
 btn_url_download.grid(row=2, column=1, pady=25, padx=40)
-btn_playlist = customtkinter.CTkButton(master=content_frame2, text="Playlist", width=150, height=50, text_color="white", font=lineup_font, fg_color="transparent")
+btn_playlist = customtkinter.CTkButton(master=content_frame2, text="Playlist", width=150, height=50, text_color="white", font=lineup_font, fg_color="transparent", image=playlist_png)
 btn_playlist.grid(row=3, column=1, pady=25, padx=40)
-btn_account = customtkinter.CTkButton(master=content_frame2, text="Compte", width=150, height=50, text_color="white", font=lineup_font, fg_color="transparent")
-btn_account.grid(row=4, column=1, pady=25, padx=40)
-btn_setting = customtkinter.CTkButton(master=content_frame2, text="Setting", width=150, height=50, text_color="white", font=lineup_font, fg_color="transparent")
-btn_setting.grid(row=5, column=1, pady=25, padx=40)
+btn_account = customtkinter.CTkButton(master=content_frame2, text="Compte", width=150, height=50, text_color="white", font=lineup_font, fg_color="transparent", image=account_png)
+btn_account.grid(row=5, column=1, pady=25, padx=40)
+btn_setting = customtkinter.CTkButton(master=content_frame2, text="Setting", width=150, height=50, text_color="white", font=lineup_font, fg_color="transparent", image=setting_png)
+btn_setting.grid(row=4, column=1, pady=25, padx=40)
 
 
 # navbar
