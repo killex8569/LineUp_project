903f98f32ac02c37a4c4c65f53a05f7ad8b73f07
killex8569 <alexandreanette@gmail.com>
2025-02-02 17:37:13 +0100

    better GUI

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from PyInstaller import *
 
 # set the default appearance
 customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme("green")
+customtkinter.set_default_color_theme("blue")
 
 # deactivate automatic scaling
 customtkinter.deactivate_automatic_dpi_awareness()
@@ -36,7 +36,7 @@ logo.grid(row=0, column=0)
 
 # Ajout du label sous le logo
 logo_label = customtkinter.CTkLabel(master=content_frame, text="LineUp", font=("Arial", 14), text_color="white")
-logo_label.grid(row=1, column=0, pady=(5, 10))
+logo_label.grid(row=1, column=0)
 
 # Création de la frame 2 (action)
 content_frame2 = customtkinter.CTkFrame(master=lineup, fg_color="#1E90FF", corner_radius=0)
@@ -45,12 +45,14 @@ content_frame2.grid_columnconfigure(1, weight=1)
 content_frame2.grid_rowconfigure(1, weight=1)
 
 # bouton frame 2
-btn_playlist = customtkinter.CTkButton(master=content_frame2, text="Playlist")
-btn_playlist.grid(row=1, column=1, pady=10, padx=40)
-btn_youtube = customtkinter.CTkButton(master=content_frame2, text="Youtube")
-btn_youtube.grid(row=2, column=1, pady=10, padx=40)
-btn_account = customtkinter.CTkButton(master=content_frame2, text="Compte")
-btn_account.grid(row=3, column=1, pady=10, padx=40)
+btn_youtube = customtkinter.CTkButton(master=content_frame2, text="Youtube", width=150, height=50)
+btn_youtube.grid(row=1, column=1, pady=25, padx=40)
+btn_playlist = customtkinter.CTkButton(master=content_frame2, text="Playlist", width=150, height=50)
+btn_playlist.grid(row=2, column=1, pady=25, padx=40)
+btn_account = customtkinter.CTkButton(master=content_frame2, text="Compte", width=150, height=50)
+btn_account.grid(row=3, column=1, pady=25, padx=40)
+btn_setting = customtkinter.CTkButton(master=content_frame2, text="setting", width=150, height=50)
+btn_setting.grid(row=4, column=1, pady=25, padx=40)
 
 
 # navbar
@@ -60,7 +62,10 @@ navbar.grid(row=0, column=1, rowspan=5, columnspan=5, sticky="nswe", padx=0, pad
 
 # Contenue principal (NAVBAR FRAME)
 navbar_entry_youtube = customtkinter.CTkEntry(master=navbar, placeholder_text="Recherche sur Youtube")
-navbar_entry_youtube.grid(padx=5)
+navbar_entry_youtube.grid(row=1, column=1, padx=5)
+
+navbar_btn = customtkinter.CTkButton(master=navbar, width=150, height=50, text="recherche")
+navbar_btn.grid(row= 1, column=2, padx=5, pady=5)
 
 
 
