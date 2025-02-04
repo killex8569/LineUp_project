7d8e29f97a35ff9a92584895dda36cba4e818480
killex8569 <alexandreanette@gmail.com>
2025-02-04 13:07:43 +0100

    update 2

--- new_main.py
+++ new_main.py
@@ -16,24 +16,8 @@ class Lineup(ctk.CTk):
         self.grid_rowconfigure(0, weight=1)  # configure grid system
         self.grid_columnconfigure(0, weight=1)
         
-        self.layoutmain = LayoutMain(master=self)
+        self.layoutmain = Layout(master=self)
         self.layoutmain.grid()
-    
-
-class LayoutMain(ctk.CTkFrame):
-    def __init__(self, master, **kwargs):
-        super().__init__(master, **kwargs)
-        self.frame = ctk.CTkFrame(master=self)
-        self.frame.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
-        
-        self.contentFrame = Layout(master=self)
-        self.contentFrame.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
-        
-    
-    
-    def change_frame(self, newFrame):
-        self.current_frame = newFrame
-            
         
 
 lineup = Lineup()
