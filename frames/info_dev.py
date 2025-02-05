4882ee113145f14a44dc35e7f51ccb06876003f1
killex8569 <alexandreanette@gmail.com>
2025-02-05 14:16:20 +0100

    adding info dev

---
+++ frames/info_dev.py
@@ -0,0 +1,7 @@
+import customtkinter
+
+class Info_Dev(customtkinter.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        self.label = customtkinter.CTkLabel(master=self)
+        
