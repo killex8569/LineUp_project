afac73ef68b72a24af96551675bc5c379763db99
killex8569 <87323180+killex8569@users.noreply.github.com>
2025-02-07 14:50:42 +0100

    delete code of Playlist.py

--- frames/Playlist.py
+++ frames/Playlist.py
@@ -3,17 +3,3 @@ import customtkinter as ctk
 class Playlist(ctk.CTkFrame):
     def __init__(self, master, playlists, **kwargs):
         super().__init__(master, **kwargs)
-
-        # frame playlist
-        self.your_playlist = ctk.CTkScrollableFrame(master)
-        self.your_playlist.grid(row=1, column=1, sticky="nsew")
-        
-        self.playlists = playlists
-        self.textbox = ctk.CTkTextbox(master=self.your_playlist)
-        self.textbox.grid(row=2, column=2)
-        
-        self.create_playlist_btn = ctk.CTkButton(master=self.your_playlist)
-        self.create_playlist_btn.grid(row=3, column=2, sticky="nsew")
-
-        for playlist in self.playlists:
-            self.textbox.insert("0.0", playlist.libelle)
--- frames/Youtube.py
+++ frames/Youtube.py
@@ -7,12 +7,14 @@ class Youtube(customtkinter.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
 
+        self.youtube_label = customtkinter.CTkLabel(master=self, text="résultat recherche youtube", font=("Arial", 16))
+        self.youtube_label.grid(row=1, column=1, sticky="nswe", padx=10, pady=10)
+
         self.youtube_entry = customtkinter.CTkEntry(master=self, corner_radius=5, placeholder_text="Recherche sur Youtube")
-        self.youtube_entry.grid(row=0, column=2, sticky="nswe", padx=5, pady=5)
+        self.youtube_entry.grid(row=2, column=1, sticky="nswe", padx=10, pady=10)
         
         self.youtube_btn = customtkinter.CTkButton(master=self, corner_radius=5, text="Recherche")
-        self.youtube_btn.grid(row=0, column=3, sticky="nswe", padx=5, pady=5)
+        self.youtube_btn.grid(row=2, column=2, sticky="nswe", padx=10, pady=10)
         
-        self.youtube_label = customtkinter.CTkLabel(master=self, text="résultat recherche youtube")
-        self.youtube_label.grid(row=1, column=2, sticky="nswe", padx=5, pady=5)
+
 
