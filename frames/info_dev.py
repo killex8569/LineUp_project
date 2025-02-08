c9df2a8a47a5a9f88535a60a87fbdad846758f37
killex8569 <alexandreanette@gmail.com>
2025-02-08 22:53:43 +0100

    adding new section in info dev

--- frames/info_dev.py
+++ frames/info_dev.py
@@ -9,6 +9,10 @@ class Info_Dev(customtkinter.CTkFrame):
 
         self.textBox = customtkinter.CTkTextbox(master=self)
         self.textBox.grid(row=2, column=1, columnspan=5, sticky="nswe", padx=10, pady=10)
+        
+        self.textBox_2 = customtkinter.CTkTextbox(master=self)
+        self.textBox_2.grid(row=3, column=1, columnspan=5, sticky="nswe", padx=10, pady=10)
 
         self.textBox.insert("0.0", "Nouveauté : \n\n1 - Utilisation de la POO comme base\n2 - Ajout de pages (Account etc... settings)")
+        self.textBox_2.insert("0.0", "Futures mise à jours : \n\n- Mise en place de playlist avec un CRUD pour les musiques")
         
